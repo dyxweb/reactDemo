567a27bf723957c6439df5f2e1b0ed4c46eb19c6
dyx <13483409091@163.com>
2019-04-21 13:32:20 +0800

    feat: add choose

--- react-demo/higher-demo/react-router/router4/src/route/second/second.js
+++ react-demo/higher-demo/react-router/router4/src/route/second/second.js
@@ -1,13 +1,73 @@
 import React from "react";
-import styles from '../../styles/index.less';
+import { Input, Button, Form } from 'antd';
 import {BrowserRouter as Router, Route, Switch, Link, Redirect} from 'react-router-dom';
 import SecondId from '../second/secondId'
 
-const Second = (props) => <div className={styles.number}>
-  第一个示例的第【1】个路由，第一个路由在第一个和第二个url里都会显示，但不在第三个显示
-  <Switch>
-    <Route path="/second/:id" component={SecondId} />
-    <Redirect to="/second/a" />
-  </Switch>
-</div>
-export default Second
+class Second extends React.component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      options: [],
+      option: '',
+      visible: true,
+    }
+  }
+
+  infoSubmit = () => {
+    this.props.form.validateFields((err,values) => {
+      if(!err){
+        this.setState({
+          options: [...this.state.options, values.option]
+        }, () => this.props.form.setFieldsValue({
+          option: '',
+        }))
+      }
+    })
+  }
+
+  chooseOption = () => {
+    this.setState({
+      visible: false,
+    });
+    const prevTime = new Date().getTime();
+    const timer = setInterval(() => {
+      const randomNumber = Math.floor(Math.random()*this.state.options.length);
+      this.setState({
+        option: this.state.options[randomNumber]
+      })
+      if (new Date().getTime() - prevTime > 5000) {
+        clearInterval(timer)
+      }
+    }, 100)
+  }
+
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    const { option, options, visible } = this.state;
+    return(
+      <div>
+        {getFieldDecorator('option', {
+          rules: [{
+              required: true,
+              message: '请输入选项',
+          }],
+        })(
+          <Input placeholder="请输入选项" style={{ width: '200px' }} />
+        )}
+        <Button type="primary" onClick={this.infoSubmit}>确定</Button>
+        {visible && <div>
+          {options.map((item,index) => <div key={index}>{item}</div>)}
+        </div>}
+        {(options.length > 1 && visible) && <Button type="primary" onClick={this.chooseOption}>选择</Button>}
+        {!visible && <div>{option}</div>}
+        <Switch>
+          <Route path="/second/:id" component={SecondId} />
+          <Redirect to="/second/a" />
+        </Switch>
+      </div>
+    )
+  }
+}
+
+const NewSecond = Form.create({ name: 'dyx' })(Second)
+export default NewSecond;
