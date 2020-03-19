9656c0d9ebdb0431716d301eb6d93053c2246ba5
dyxweb <13483409091@163.com>
2020-03-19 14:21:29 +0800

    docs: 删除文件

--- conclusion/next-form/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import '@alifd/next/dist/next.css';
-import './index.css';
-import App from './routes/index.js';
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
